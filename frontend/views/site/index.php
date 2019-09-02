2df06dca0089526a376c571f73d7a7d93bdfb9fc
Clown <rd@promer.co>
2019-09-02 22:43:57 +0800

    init

---
+++ frontend/views/site/index.php
@@ -0,0 +1,30 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'P';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>P</h1>
+
+        <p class="lead">Promer Sys Boot</p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+
+    </div>
+</div>
